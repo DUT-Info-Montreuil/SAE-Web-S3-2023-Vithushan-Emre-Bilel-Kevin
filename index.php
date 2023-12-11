198f772a1b19e582c1ca5c46822a69bfbd8ed934
bhadadi <bilelhadadi2208@gmail.com>
2023-12-11 10:02:38 +0100

    ajout main et footer

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SpaceTowerDefesne</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Space Tower Defense</h1>
+    </header>
+    <nav>
+        <a href="">Classement</a>
+        <a href="">Support</a>
+        <a class="test" href="">Jouer</a>
+        <a href="">Clan</a>
+        <a href="">guide</a>
+    </nav>
+    <main>
+
+    </main>
+    <footer>
+        
+    </footer>
+</body>
+</html>
