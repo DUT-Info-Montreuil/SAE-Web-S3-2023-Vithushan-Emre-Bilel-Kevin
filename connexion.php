833ca1687d4187fe2709e519bc4e3453146d202c
vvith <vvijayatharan@iut.univ-paris8.fr>
2023-12-14 22:00:16 +0100

    configuration de connexion

--- connexion.php
+++ connexion.php
@@ -1,1 +1,14 @@
 <?php
+class Connexion {
+
+    protected static $bdd;
+
+    public function init_connection() {
+        $lien = "mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201691";
+        $utilisateur = "dutinfopw201691";
+        $mdp = "bygydasa";
+        self::$bdd = new PDO ($lien,$utilisateur,$mdp);
+    }
+}
+
+?>
