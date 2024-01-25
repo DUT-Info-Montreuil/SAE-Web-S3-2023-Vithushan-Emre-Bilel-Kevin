7d9116410d1dbb7618456abbe5f04a2e8bee06da
emrebondy <120063635+emrebondy@users.noreply.github.com>
2024-01-25 16:54:58 +0100

    profil Ameliorer

--- modules/mod_profil/modele_profil.php
+++ modules/mod_profil/modele_profil.php
@@ -37,6 +37,13 @@ class ModeleProfil extends Connexion {
         
     }
 
+    public function recupereIdClan() {
+        $idJoueur = $this->infoJoueur();
+        $idClan = self::$bdd->prepare('select idClan from clan INNER JOIN appartient USING(idClan) WHERE idUtilisateur=:idUtilisateur');
+        $idClan->execute([':idUtilisateur' => $idJoueur['idUtilisateur']]);
+        return $idClan->fetch(); 
+    }
+
 
     public function changerLeMdp(){
         $AncienMdp = isset($_POST['ancienMDP']) ? $_POST['ancienMDP'] : null;
@@ -67,11 +74,43 @@ class ModeleProfil extends Connexion {
             ?>
                 <p><?= "Mot de passe incorect" ?></p>
             <?php
+        }
+    }
+
+
+                    
+
+    public function changerLeMail(){
+        $mdp = isset($_POST['motDePasse']) ? $_POST['motDePasse'] : null;
+        $user = $this->infoJoueur();
+        $pwhash = $user['motdepasse'];
+        $newMail = isset($_POST['newMail']) ? $_POST['newMail'] : null;
+        $newMail2 = isset($_POST['newMail2']) ? $_POST['newMail2'] : null;
+
+        if (password_verify($mdp,$pwhash) AND !empty($newMail) AND !empty($newMail2)){
+            if($newMail === $newMail2){
+                $requeteUpdate = self::$bdd->prepare('UPDATE utilisateur SET adressemail = :newMail WHERE nom = :nomUtilisateur');
+
+                $requeteUpdate->execute(['newMail' => $newMail,'nomUtilisateur' => $user['nom']]);
+
+                echo 'Le mail à était modifié avec succés';
+
+            }
+            else{
+                ?>
+                <p><?= "Vous avez pas ecrit deux fois le même mail" ?></p>
+                <?php
+            }
             
         }
-
-        
+        else{
+            ?>
+                <p><?= "Mot de passe incorect" ?></p>
+            <?php
+        }
     }
+
+    
 
 
 
