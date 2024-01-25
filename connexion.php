94a26b6ab2940a13f4a807675a6d5c80ffe083cc
vithu <vvijayatharan@iut.univ-paris8.fr>
2024-01-25 23:41:50 +0100

    fin

--- connexion.php
+++ connexion.php
@@ -6,6 +6,8 @@ class Connexion {
     public static function init_connexion() {
         self::$bdd = new PDO ('mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201691', 'dutinfopw201691', 'bygydasa');
 
+
+
     }
 }
 
